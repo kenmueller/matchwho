63b63e6f8e76f8939f42623bb2b23674aee73044
Ken Mueller <kenmueller0@gmail.com>
2023-03-18 12:55:20 -0700

    header style options

--- lib/header/options.ts
+++ lib/header/options.ts
@@ -1,9 +1,22 @@
+import { StyleSheet } from 'react-native'
+
 import theme from '../theme'
+
+const styles = StyleSheet.create({
+	header: {
+		backgroundColor: theme.darkGray
+	},
+	headerTitle: {
+		fontSize: 20,
+		fontWeight: '700'
+	}
+})
 
 const HEADER_OPTIONS = {
 	headerTitleAlign: 'center',
 	headerTintColor: theme.white,
-	headerStyle: { backgroundColor: theme.darkGray }
+	headerStyle: styles.header,
+	headerTitleStyle: styles.headerTitle
 } as const
 
 export default HEADER_OPTIONS
