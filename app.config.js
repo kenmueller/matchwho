96c37dbe7a0ebc4cd383d75304e0643baeb1b81f
Ken Mueller <kenmueller0@gmail.com>
2023-03-18 18:04:22 -0700

    eas config

--- app.config.js
+++ app.config.js
@@ -1,6 +1,10 @@
 module.exports = ({ config }) => ({
 	...config,
+	updates: {
+		url: `https://u.expo.dev/${process.env.EAS_PROJECT_ID}`
+	},
 	extra: {
+		eas: { projectId: process.env.EAS_PROJECT_ID },
 		API_ORIGIN: process.env.API_ORIGIN
 	}
 })
