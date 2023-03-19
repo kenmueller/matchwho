33131ae9999e4291bec1990643cdc7390b78aa85
Ken Mueller <kenmueller0@gmail.com>
2023-03-19 13:38:41 -0700

    added ios url

--- lib/apps.ts
+++ lib/apps.ts
@@ -1,2 +1,2 @@
-export const IOS_URL = 'https://api.matchwho.io' // TODO: Add actual iOS url
+export const IOS_URL = 'https://apps.apple.com/us/app/match-who/id6446462042'
 export const ANDROID_URL = 'https://api.matchwho.io' // TODO: Add actual Android url
