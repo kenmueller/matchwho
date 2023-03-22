a847467987cae25c42fa121ba41ffd35877a00ca
Ken Mueller <kenmueller0@gmail.com>
2023-03-22 14:31:45 -0700

    removed socket origin

--- lib/api/origin.ts
+++ lib/api/origin.ts
@@ -1,6 +1,5 @@
 import Constants from 'expo-constants'
 
 const API_ORIGIN = Constants.expoConfig!.extra!.API_ORIGIN as string
-export const SOCKET_ORIGIN = API_ORIGIN.replace('http', 'ws')
 
 export default API_ORIGIN
